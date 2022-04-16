4e26c456de6b9690dff9dbdc6b71fd0c3601cf6e
Amenacliff <amenaclifford@gmail.com>
2022-04-16 19:06:12 +0100

    Worked on  Creating the Nodes & a few receiver Functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module doublyLinkedListImpl
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import "log"
+
+type Node struct {
+	data int
+	next *Node
+	prev *Node
+}
+
+type DoublyLinkedList struct {
+	length int
+	head   *Node
+	tail   *Node
+}
+
+func (d *DoublyLinkedList) InitializeList() *DoublyLinkedList {
+	return &DoublyLinkedList{}
+}
+
+func (d *DoublyLinkedList) AddNodeToFront(data int) {
+	newNode := &Node{
+		data: data,
+	}
+
+	if d.head == nil {
+		d.head = newNode
+		d.tail = newNode
+	} else {
+		newNode.next = d.head
+		d.head.prev = newNode
+		d.head = newNode
+	}
+
+	d.length++
+}
+
+func (d *DoublyLinkedList) DisplayAllItemsInList() {
+	currentNode := d.head
+	for currentNode != nil {
+		log.Println(currentNode.data)
+		currentNode = currentNode.next
+	}
+}
+
+func main() {
+	var linkedList = DoublyLinkedList{}
+	linkedList.InitializeList()
+	linkedList.AddNodeToFront(5)
+	linkedList.AddNodeToFront(7)
+	linkedList.AddNodeToFront(7)
+	linkedList.AddNodeToFront(6)
+	linkedList.AddNodeToFront(2)
+	linkedList.AddNodeToFront(1)
+	linkedList.DisplayAllItemsInList()
+}
